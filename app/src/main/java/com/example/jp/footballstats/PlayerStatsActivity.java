0e27656928d781eac4b6777fe13ef57284086687
petrjech <blunderer@volny.cz>
2016-07-05 14:25:48 +0200

    Delete player menu shows only in PlayerStats activity.

--- app/src/main/java/com/example/jp/footballstats/PlayerStatsActivity.java
+++ app/src/main/java/com/example/jp/footballstats/PlayerStatsActivity.java
@@ -47,7 +47,7 @@ public class PlayerStatsActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_player_stats, menu);
         return true;
     }
 
